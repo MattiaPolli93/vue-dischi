4d3be853bb19302b80bda844c6c38d348675fa5b
Mattia <matpolli93@gmail.com>
2021-04-08 18:38:17 +0200

    Add bonus task #2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const app = new Vue({
     data: {
         selected: "",
         musicAlbums: [],
-        orderDesc: true
+        order: true
     },
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((result) => {
@@ -20,8 +20,35 @@ const app = new Vue({
                 return this.musicAlbums.filter((item) => item.genre === this.selected);
             }
         }
+    },
+    methods: {
+        sortByOldestYear() {
+            if (this.order) {
+                this.musicAlbums.sort((a, b) => {
+                    if (a.year < b.year) {
+                        return -1;
+                    } else if (a.year > b.year) {
+                        return 1;
+                    }
+                    return 0;
+                });
+
+                this.order = false;
+            }                
+        },
+        sortByRecentYear() {
+            if (this.order === false) {
+                this.musicAlbums.sort((a, b) => {
+                    if (a.year > b.year) {
+                        return -1;
+                    } else if (a.year < b.year) {
+                        return 1;
+                    }
+                    return 0;
+                });
+
+                this.order = true;
+            }
+        }
     }
 });
-
-// 1 nel mounted nel then
-// 2 BUtton 
