d8b609040709df6b9d190c4c8124911154ca300c
Mattia <matpolli93@gmail.com>
2021-04-08 17:38:10 +0200

    Add img folder and bonus task #1

--- js/script.js
+++ js/script.js
@@ -3,11 +3,25 @@
 const app = new Vue({
     el: "#app",
     data: {
-        musicAlbums: []
+        selected: "",
+        musicAlbums: [],
+        orderDesc: true
     },
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((result) => {
             this.musicAlbums = result.data.response;
         });
+    },
+    computed: {
+        filterMusic() {
+            if (this.selected === "") {
+                return this.musicAlbums;
+            } else {
+                return this.musicAlbums.filter((item) => item.genre === this.selected);
+            }
+        }
     }
 });
+
+// 1 nel mounted nel then
+// 2 BUtton 
